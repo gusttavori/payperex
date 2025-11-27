e990ee7861d45fa7e789f21b3e05559edeca57d6
gusttavori <gusta.contato@outlook.com>
2025-11-27 15:20:06 -0300

    Atualização

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+// Aqui está a mágica: ele tenta pegar a variável da Vercel. 
+// Se não achar (no seu PC), usa o localhost.
 const api = axios.create({
-  baseURL: 'https://payperex.onrender.com', // Endereço do seu Backend
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
 });
 
 // Antes de cada requisição, verifica se tem token salvo e adiciona no cabeçalho
